55e2cefb9acf168475e89e931555406719d2b568
Christopher Erick Moody <cemoody@squareup.com>
2014-06-25 15:11:04 -0700

    added vim mode

--- custom.js
+++ custom.js
@@ -49,3 +49,8 @@
  */
 
 IPython.Cell.options_default.cm_config.lineNumbers = true;
+// add this to <profile>/static/custom/custom.js to load vim keybindings:
+$.getScript("/static/components/codemirror/keymap/vim.js", function() {
+    if (! IPython.Cell) return;
+    IPython.Cell.options_default.cm_config.keyMap = "vim";
+});
